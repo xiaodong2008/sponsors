c7e7a7064a96de6390298a8bf28b2ebb9b46b85d
xiaodong2008 <admin@xiaodong.moe>
2024-05-06 01:20:10 +0800

    update version

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Thanks for your support, every sponsor will be listed here and also on
 the [GitHub Profile](https://github.com/xiaodong2008).
 
+If my work helps you, you can sponsor me via [GitHub Sponsors](https://github.com/sponsors/xiaodong2008) or [AfDian](https://afdian.net/a/xiaodong2008), this will help me to continue my work.
+
 ### All Sponsors
 
 <div align="center">
--- sponsor.config.ts
+++ sponsor.config.ts
@@ -1,35 +1,51 @@
-import {defineConfig, presets} from "sponsorkit";
+import { defineConfig, tierPresets } from "sponsorkit";
 
 export default defineConfig({
-    formats: ['svg', 'json'],
-    tiers: [
-        {
-            title: 'Past Sponsors',
-            monthlyDollars: -1,
-            preset: presets.xs,
-        },
-        {
-            title: 'Backers',
-            preset: presets.base,
-        },
-        {
-            title: 'Sponsors',
-            monthlyDollars: 8,
-            preset: presets.medium,
-            // to insert custom elements after the tier block
-            composeAfter: (composer, _tierSponsors, _config) => {
-                composer.addSpan(10)
-            },
-        },
-        {
-            title: 'Silver Sponsors',
-            monthlyDollars: 16,
-            preset: presets.large,
-        },
-        {
-            title: 'Gold Sponsors',
-            monthlyDollars: 32,
-            preset: presets.xl,
-        },
-    ]
+  formats: ['svg', 'json'],
+  afdian: {
+    exechangeRate: 7.5,
+  },
+  tiers: [
+    {
+      title: 'Past Sponsors',
+      monthlyDollars: -1,
+      preset: tierPresets.xs,
+    },
+    {
+      title: 'Backers',
+      preset: tierPresets.base,
+    },
+    {
+      title: 'Sponsors',
+      monthlyDollars: 8,
+      preset: tierPresets.medium,
+      composeAfter: (composer, _tierSponsors, _config) => {
+        composer.addSpan(10)
+      },
+    },
+    {
+      title: 'Silver Sponsors',
+      monthlyDollars: 16,
+      preset: tierPresets.large,
+    },
+    {
+      title: 'Gold Sponsors',
+      monthlyDollars: 32,
+      preset: tierPresets.xl,
+    },
+  ],
+  renders: [
+    {
+      name: 'sponsors',
+      renderer: 'tiers',
+    },
+    {
+      name: 'sponsors.wide',
+      width: 1000,
+    },
+    {
+      name: 'sponsors.circles',
+      renderer: 'circles',
+    },
+  ],
 })
